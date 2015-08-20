8120d57dded94596e0830edc7de34f3c8ee973f8
William Cheung <williamcheung.thu@gmail.com>
2015-08-20 10:57:48 -0400

    Update GNETTypes.h

--- GNETTypes.h
+++ GNETTypes.h
@@ -2,13 +2,13 @@
 #define __GNET_TYPES_H__
 
 namespace YChat {
-	typedef GNET::Octets   					Octets;
+	typedef GNET::Octets   			Octets;
 	typedef GNET::Marshal::OctetsStream 	OctetsStream;
-	typedef GNET::Protocol 					Protocol;
-	typedef GNET::Protocol::Manager			Manager;
-	typedef GNET::Conf						Conf;
-	typedef GNET::PollIO					PollIO;
-	typedef GNET::Thread::Pool				ThreadPool;
+	typedef GNET::Protocol 			Protocol;
+	typedef GNET::Protocol::Manager		Manager;
+	typedef GNET::Conf			Conf;
+	typedef GNET::PollIO			PollIO;
+	typedef GNET::Thread::Pool		ThreadPool;
 
 	typedef GNET::Protocol::Manager::Session::ID 	sid_t;
 	typedef GNET::Protocol::Manager::Session::State session_state_t;
